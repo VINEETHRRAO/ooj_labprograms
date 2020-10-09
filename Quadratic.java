d4a84e9126b1154e7ed17613170c5f63758e9cd3
VINEETHRRAO <71490722+VINEETHRRAO@users.noreply.github.com>
2020-10-09 15:50:10 +0530

    Add files via upload

---
+++ Quadratic.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+import java.lang.*;
+ class Quadratic{
+public static void main(String[] args)
+{double a,b,c,r1,r2,d,real,imag;
+
+Scanner scan= new Scanner(System.in);
+System.out.println("enter the co-efficients of the quadratic equation a,b and c\n");
+ a = scan.nextDouble();
+ b = scan.nextDouble();
+ c = scan.nextDouble();
+d = b*b- (4*a*c);
+
+r1 = (-b + Math.sqrt(d))/(2*a);
+r2 = (-b - Math.sqrt(d))/(2*a);
+
+
+
+if(d<0){
+real=-b;
+imag=d;
+System.out.println("the roots are imaginary:"+(real)+"+("+(+1.0*imag)+"i),"+(real)+"+("+(-1.0*imag)+"i)");
+}
+
+else if(d==0)
+{ System.out.println("The solutions are real and equal to " + r1 );
+}
+else 
+{System.out.println("The real solutions are\n" + r1 + " and " + r2);}
+
+}
+}
+
+
